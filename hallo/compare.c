7a93e96e0d5536334a72bbd86dac14c539058b29
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-21 10:48:07 +0000

    Fri, Oct 21, 2022, 10:48 AM +00:00

--- hallo/compare.c
+++ hallo/compare.c
@@ -10,8 +10,15 @@ int main(void)
     {
         printf("x is less than y\n");
     }
+
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+
     else
     {
-        printf("x is not less than y\n");
+        printf("x is equal to y"as )
     }
+
 }
