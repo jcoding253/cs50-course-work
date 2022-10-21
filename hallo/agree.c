c2159e5581d6faa35a6712963ec2bfb05a606ca4
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-21 11:34:52 +0000

    Fri, Oct 21, 2022, 11:34 AM +00:00

--- hallo/agree.c
+++ hallo/agree.c
@@ -3,5 +3,16 @@
 
 int main(void)
 {
-    char c = get_char("Do you agree my butt smells? ")
+    char c = get_char("Do you agree my butt smells? ");
+
+    if (c == 'y')
+    {
+        printf("Agreed.\n");
+    }
+
+    if (c == 'n')
+    {
+        printf("Disagreed.\n");
+    }
 }
+
