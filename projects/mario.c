cee4769f68b0fd02952725d40d76f9d6f97f480d
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-23 02:12:06 +0000

    Sun, Oct 23, 2022, 2:12 AM +00:00

--- projects/mario.c
+++ projects/mario.c
@@ -4,25 +4,21 @@
 int main(void)
 {
     printf("\n");
-
     for (int i = 0; i < 4; i++)
     {
         printf("?");
     }
 
     printf("\n\n");
-
     for (int b = 0; b < 3; b++)
     {
         printf("#\n");
     }
 
     printf("\n");
-
     const int n = 3;
     for (int c = 0; c < n; c++)
     {
-
         for (int r = 0; r < n; r++)
         {
             printf("#");
@@ -31,5 +27,4 @@ int main(void)
     }
 
     printf("\n");
-
 }
