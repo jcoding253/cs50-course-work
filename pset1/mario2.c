76b9fc2114f57f0ed08dd5b171e538f4af811a7b
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-24 03:35:49 +0000

    Mon, Oct 24, 2022, 3:35 AM +00:00

---
+++ pset1/mario2.c
@@ -0,0 +1,68 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // row of question marks
+    printf("\n");
+    for (int i = 0; i < 4; i++)
+    {
+        printf("?");
+    }
+
+    // column of $'s
+    printf("\n\n");
+    for (int i = 0; i < 2; i++)
+    {
+        printf("$\n");
+    }
+
+    // constant 3x3 blocks
+    printf("\n");
+    const int n = 3;
+    for (int c = 0; c < n; c++)
+    {
+        for (int r = 0; r < n; r++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+
+    printf("\n");
+    int get_size("Size: ");
+
+    printf("\n");
+    print_grid(int s)
+    printf("\n");
+}
+
+//abstraction for get_size
+// get size of grid, conditional loop
+int get_size (void)
+{
+    printf("\n");
+    int s;
+    do
+    {
+        s = get_int("Size: ");
+    }
+    while (s < 1)
+    return s;
+}
+
+//abstraction for printing
+// print previous grid with stars
+void print_grid(int s)
+{
+    printf("\n");
+    for (int c = 0; c < s; c++)
+    {
+        for (int r = 0; r < s; r++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
