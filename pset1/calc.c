af2078368663cae47fb289cfa7ecca6b08792d0d
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-24 04:28:34 +0000

    Mon, Oct 24, 2022, 4:28 AM +00:00

--- pset1/calc.c
+++ pset1/calc.c
@@ -3,10 +3,15 @@
 
 int main(void)
 {
+    //calculate with big integers
     long x = get_long("What's x? ");
     long y = get_long("What's y? ");
 
     printf("%li\n", x + y);
 
-    
+    //calculate with decimals
+    float x = get_float("What's x? ");
+    float y = get_float("What's y? ");
+
+    printf("%f\n", x / y);
 }
