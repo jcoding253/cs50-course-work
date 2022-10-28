b6d636e3c878f0c826236b4d979d015c6d03a2ba
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-28 07:23:07 +0000

    Fri, Oct 28, 2022, 7:23 AM +00:00

---
+++ pset/scores.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+const int Y = 3
+
+int main(void)
+{
+    int score[Y]
+    for (int i = 0; i < Y; i++)
+    {
+        score[Y] = get_int("Score: ");
+    {
+
+    printf("Average: %f\n", int average{});
+{
+
+float average (int A[])
+{
+    int sum = 0
+    for (int i = 0, i < Y, i++)
+    {
+        sum += A[i];
+    }
+    return sum / (float) Y;
+}
