1d1b9e8d2a20e9e18a8ca0252b2ffe083bd0ccad
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-29 22:20:31 +0000

    Sat, Oct 29, 2022, 10:20 PM +00:00

--- pset/search.c
+++ pset/search.c
@@ -1,34 +1,37 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <stirng.h>
+#include <string.h>
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
-
-    int n = get_int("Number: ");
-    for (int i = 0; i < 7; i++)
     {
-        if (numbers[i] == n)
+        int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+        int n = get_int("Number: ");
+        for (int i = 0; i < 7; i++)
         {
-            printf("Found\n");
-            return 0;
+            if (numbers[i] == n)
+            {
+                printf("Found\n");
+                return 0;
+            }
         }
+        printf("Not found\n");
+        return 1;
     }
-    printf("Not found\n");
-    return 1;
-}
 
-{
-    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-    string s = get_string("String: ");
-    for (int i = 0; i < 7; i++)
     {
-        if (strcmp(strings[i], s) == 0)
+        string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+        string s = get_string("String: ");
+        for (int i = 0; i < 7; i++)
         {
-            printf("Found\n");
-            return 0;
+            if (strcmp(strings[i], s) == 0)
+            {
+                printf("Found\n");
+                return 0;
+            }
         }
     }
 }
