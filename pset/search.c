30656612788b2a859cadfb44169587524fd41165
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-29 21:33:56 +0000

    Sat, Oct 29, 2022, 9:33 PM +00:00

---
+++ pset/search.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+
+    int n = get_int("Number: ");
+    {
+        if (numbers[i] == n)
+        {printf}
+    }
+
+}
