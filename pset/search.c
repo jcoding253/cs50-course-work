9013964a9ec1c37373d0a9511595b10d241d2991
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-29 21:37:38 +0000

    Sat, Oct 29, 2022, 9:37 PM +00:00

--- pset/search.c
+++ pset/search.c
@@ -6,9 +6,14 @@ int main(void)
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
     int n = get_int("Number: ");
+    for (int i = 0; i < 7; i++)
     {
         if (numbers[i] == n)
-        {printf}
+        {
+            printf("Found\n");
+            return 0;
+        }
     }
-
+    printf("Not found\n");
+    return 1;
 }
