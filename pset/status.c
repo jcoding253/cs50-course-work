8018aca52adc45b614662bf513f6ed2bdf5cb67d
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-29 06:51:05 +0000

    Sat, Oct 29, 2022, 6:51 AM +00:00

--- pset/status.c
+++ pset/status.c
@@ -6,9 +6,13 @@ int main (int argc, string argv[])
     if (argc != 2)
     {
         printf("Missing command-line argument\n");
+        return 10;
     }
+
     else
     {
         printf("Hey, %s\n", argv[1]);
+        return 11;
     }
+
 }
