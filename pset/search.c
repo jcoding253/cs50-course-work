2975024fe30e6fcb5a53d5685a9a71a60cbb0932
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-10-30 06:29:24 +0000

    Sun, Oct 30, 2022, 6:29 AM +00:00

--- pset/search.c
+++ pset/search.c
@@ -20,24 +20,5 @@ int main(void)
         printf("Not found\n");
         return 1;
     }
-    {
-        //algorithm for string search
-        string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-        string s = get_string("String: ");
-        for (int i = 0; i < 6; i++)
-        {
-            if (strcmp(strings[i], s) == 0)
-            {
-                                printf("Found\n");
-                return 2;
-            }
-        }
-                printf("Not found\n");
-        return 3;
-        {
-
-    }
-
-    {
-{
+}
