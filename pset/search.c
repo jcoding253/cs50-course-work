becbd55122990d38d3128b52e0db56e896a222dc
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-11-01 22:54:27 +0000

    Tue, Nov 1, 2022, 10:54 PM +00:00

--- pset/search.c
+++ pset/search.c
@@ -4,21 +4,6 @@
 
 int main(void)
 {
-    //algorithm for string search
-    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
-
-    string s = get_string("String: ");
-    for (int i = 0; i < 6; i++)
-    {
-        if (strcmp(strings[i], s) == 0)
-        {
-            printf("Found\n");
-            return 3;
-        }
-    }
-    printf("Not found\n");
-
-
     //algorithim for number search
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
@@ -28,9 +13,20 @@ int main(void)
         if (numbers[i] == n)
         {
             printf("Found\n");
-            return 2;
         }
     }
     printf("Not found\n");
-    return 1;
+
+    //algorithm for string search
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
+
+    string s = get_string("String: ");
+    for (int i = 0; i < 6; i++)
+    {
+        if (strcmp(strings[i], s) == 0)
+        {
+            printf("Found\n");
+        }
+    }
+    printf("Not found\n");
 }
