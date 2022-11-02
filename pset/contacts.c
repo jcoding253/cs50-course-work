34f0d8dbfffb4d8f39202a062a8e21ade55319ad
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-11-02 00:23:12 +0000

    Wed, Nov 2, 2022, 12:23 AM +00:00

--- pset/contacts.c
+++ pset/contacts.c
@@ -22,7 +22,7 @@ int main(void)
     string name = get_string("Name ");
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(peopele[i].name, name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
             printf("Found %s\n", people[i].number);
             return 0;
