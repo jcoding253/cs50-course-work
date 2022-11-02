fa5e4067ab9128ffcef01599d8e05af6a6966e0f
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-11-02 00:16:29 +0000

    Wed, Nov 2, 2022, 12:16 AM +00:00

--- pset/contacts.c
+++ pset/contacts.c
@@ -1,1 +1,31 @@
-#include
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
+int main(void)
+{
+    person people[2];
+
+    people[0].name = "Sammi";
+    people[0].number = "+1-832-234-6456";
+
+    people[1].name = "Jesse";
+    people[1].number = "+1-347-623-7635";
+
+    string name = get_string("Name ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(peopele[i].name, name) == 0)
+        {
+            printf("Found %s\n", people[i].number);
+        }
+
+    }
+}
