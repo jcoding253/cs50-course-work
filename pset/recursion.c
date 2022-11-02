ae213e2cbcc73c83201732a3951d5469d08386dd
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-11-02 00:35:14 +0000

    Wed, Nov 2, 2022, 12:35 AM +00:00

--- pset/recursion.c
+++ pset/recursion.c
@@ -5,7 +5,7 @@ void draw(int n);
 
 int main(void)
 {
-
+    draw (1);
 }
 
 void draw(int n)
@@ -13,4 +13,8 @@ void draw(int n)
     draw(n - 1);
 
     for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
