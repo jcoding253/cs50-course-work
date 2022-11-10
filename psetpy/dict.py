e9f57f7f6b198c61e27ed01922d8a103feace6dc
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-11-10 23:12:48 +0000

    Thu, Nov 10, 2022, 11:12 PM +00:00

---
+++ psetpy/dict.py
@@ -0,0 +1,20 @@
+
+
+words = set()
+
+
+def check(word):
+    if word.lower() in words:
+        returns True
+    else:
+        return False
+
+
+def load(dictionary)
+
+
+def size():
+
+
+def unload():
+
