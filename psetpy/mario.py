98fc42ff52400b679ce450af5c68237e6f1f4ae2
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-03 08:02:08 +0000

    Sat, Dec 3, 2022, 8:02 AM +00:00

--- psetpy/mario.py
+++ psetpy/mario.py
@@ -1,19 +1,17 @@
-from cs50 import get_int
-
-
+//Calls get height function
 def main():
     height = get_height()
     for i in range (height):
         print('#')
 
-
+//Tries the values until an integer enters
 def get_height():
     while True:
         try:
             n = int(input('Height: '))
             if n > 0:
                 return n
-            except ValueError:
-                print('Not an integer, please try again. ')
+        except ValueError:
+            print('Not an integer, please try again. ')
 
 main()
