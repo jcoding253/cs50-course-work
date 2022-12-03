5d4d27051e978f40d247cace966d2836766cfe41
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-03 07:48:46 +0000

    Sat, Dec 3, 2022, 7:48 AM +00:00

--- psetpy/mario.py
+++ psetpy/mario.py
@@ -1,8 +1,14 @@
 from cs50 import get_int
 
-def main()
+def main():
     height = get_height
-    for i in range (height)
+    for i in range (height):
         print('#')
 
-def get 
+def get_height():
+    while True:
+        n = get_int('Height: ')
+        if n > 0:
+            return n
+
+main()
