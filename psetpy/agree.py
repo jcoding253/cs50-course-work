87969f8b436f75a4de39cde7d4bdc9f155b649a0
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-03 07:26:18 +0000

    Sat, Dec 3, 2022, 7:26 AM +00:00

---
+++ psetpy/agree.py
@@ -0,0 +1,7 @@
+
+s = input('Do you agree? ')
+
+if s in ('Y' , 'y')
+    print ('Agreed.')
+elif s in ('N' , 'n')
+    print ('Not Agreed.')
--- psetpy/compare.py
+++
@@ -1,5 +0,0 @@
-
-
-x = int(input("x: "))
-y = int(input("y: "))
-
