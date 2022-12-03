9f2ffbbf4d71f0d3a41129373c29f031d87908c8
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-03 08:33:31 +0000

    Sat, Dec 3, 2022, 8:33 AM +00:00

--- psetpy/scores.py
+++ psetpy/scores.py
@@ -3,7 +3,7 @@ from cs50 import get_int
 scores = []
 for i in range(3):
     score = get_int('Score: ')
-    scores.append(score)
+    scores += [score]
 
 average = sum(scores) / len(scores)
 print(f'Average: {average}' )
