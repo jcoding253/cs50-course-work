dc8593e788412520eb936cf3e579a22fda690497
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-11 23:12:24 +0000

    Sun, Dec 11, 2022, 11:12 PM +00:00

--- psetpy/agree.py
+++ psetpy/agree.py
@@ -1,11 +1,21 @@
 
 s = input('Do you agree? ')
 
-
 //A Method that creates a lowercase function within the string
 s = s.lower()
 
 if s in ('y' , 'yes')
     print ('Agreed.')
 elif s in ('n' , 'no')
+    print ('Not Agreed.')
+
+
+//regular expressions
+import re
+
+s = s.lower()
+
+if re.search('^y(es)?$', s):
+    print ('Agreed.')
+elif re.search('^no?$', s):
     print ('Not Agreed.')
