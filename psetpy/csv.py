7aae9e580e79cd443ac6b9ff3898c283d2130419
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-11 22:54:23 +0000

    Sun, Dec 11, 2022, 10:54 PM +00:00

--- psetpy/csv.py
+++ psetpy/csv.py
@@ -1,10 +1,10 @@
 import csv
 
-file = open('phonebook.csv', 'a')
+with open('phonebook.csv', 'a') as file:
 
-name = input('Name: ')
-number = input('Number: ')
+    name = input('Name: ')
+    number = input('Number: ')
 
-writer = csv.writer(file)
-writer.writerow([name, number])
+    writer = csv.writer(file)
+    writer.writerow([name, number])
 
