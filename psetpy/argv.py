369adedd1f198583a1df11061ffd5e462cd194f2
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-11 19:21:31 +0000

    Sun, Dec 11, 2022, 7:21 PM +00:00

--- psetpy/argv.py
+++ psetpy/argv.py
@@ -1,12 +1,12 @@
 import sys
 
-if len(sys.argv) != 2:
+if len(argv) != 2:
     print('Missing command-line argument of 2 args')
     sys.exit(1)
 
 for arg in argv[1:]:
     print(arg)
 
-print(f'Hello, {sys.atgv[1]}')
+print(f'Hello, {argv[1]}')
 sys.exit(0)
 
