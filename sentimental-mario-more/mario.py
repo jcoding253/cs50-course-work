9fd03c6536d0fbf862f4cb4d6fefbe9a4e82716b
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-18 08:20:02 +0000

    Sun, Dec 18, 2022, 8:20 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -21,9 +21,7 @@ def draw(i):
     draw(i-1)
 
     for i in range (i):
-        for i in range (i):
-            print('#', end='')
-        print('#', end='')
+        print('#  #', end='')
 
     print()
 
