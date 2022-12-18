ab4b7654597fa2678286a46358c5f2f6b0e6a866
jchurch133 <116246291+jchurch133@users.noreply.github.com>
2022-12-18 03:50:00 +0000

    Sun, Dec 18, 2022, 3:50 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,2 +1,17 @@
 # TODO
 
+def main():
+    height = get_height()
+    for i in range (height):
+        print('#')
+
+def get_height():
+    while True:
+        try:
+            n = int(input('Height: '))
+            if n > 0:
+                return n
+        except ValueError:
+            print('Not an integer, please try again. ')
+
+main()
